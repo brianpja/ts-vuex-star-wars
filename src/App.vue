bdabbfeea6aa2902421c29a71e789162514e7a30
Brian James <brian.james@slalom.com>
2020-06-19 14:14:40 -0700

    css changes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
       <router-link to="/favorites">Favorites</router-link>
     </div>
     <router-view />
@@ -19,11 +19,21 @@
 }
 
 #nav {
-  padding: 30px;
+  text-align: left;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    padding: 20px 50px;
+    text-decoration: none;
+    color: green;
+
+    &:hover {
+      background-color: grey;
+    }
 
     &.router-link-exact-active {
       color: #42b983;
