a851b5a551c741e6a490e2dc11661096e1440d5e
Brian James <brian.james@slalom.com>
2020-06-23 14:55:12 -0700

    modified code to be typed

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,12 +3,12 @@
     <h1>This is an about page</h1>
     <button @click="showModal">Show Modal</button>
     <Modal v-if="this.$store.state.showModal" />
-    <custom-datepicker 
-      @dateSelected  = "setDate($event)"
-      :date          = "selectedDate" 
-      :primaryColor  = "primaryColor"
-      :wrapperStyles = "wrapperStyles"
-      :limits        = "limits"
+    <custom-datepicker
+      @dateSelected="setDate($event)"
+      :date="selectedDate"
+      :primaryColor="primaryColor"
+      :wrapperStyles="wrapperStyles"
+      :limits="limits"
     />
     <h2>{{ selectedDate }}</h2>
   </div>
@@ -27,18 +27,27 @@ import moment from "moment";
   }
 })
 export default class About extends Vue {
-  selectedDate = moment().format('YYYY-MM-DD');
-  wrapperStyles = { width: '325px' };
-  primaryColor = "#0918bc";
-  limits = {
-    start: '2017-04-02',
-    end: '2017-05-22'
-  };
+  selectedDate: string;
+  wrapperStyles: object;
+  primaryColor: string;
+  limits: object;
+
+  constructor() {
+    super();
+    this.selectedDate = moment().format('YYYY-MM-DD');
+    this.wrapperStyles = { width: '325px' };
+    this.primaryColor = "#0918bc";
+    this.limits = {
+      start: '2017-04-02',
+      end: '2017-05-22'
+    };
+  }
+  
   showModal() {
     this.$store.commit("showModal");
   }
 
-  setDate(date: Date) {
+  setDate(date: string) {
     this.selectedDate = date;
   }
 }
